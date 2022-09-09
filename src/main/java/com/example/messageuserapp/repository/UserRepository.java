3d11e4810310678fd6450bb960abfa089ce1b227
DonBahall <dimachar2004@gmail.com>
2022-09-09 09:50:08 +0200

    update

--- src/main/java/com/example/messageuserapp/repository/UserRepository.java
+++ src/main/java/com/example/messageuserapp/repository/UserRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserModel,Long> {
     Optional<UserModel> findByUsernameAndPassword(String username, String password);
-
 }
